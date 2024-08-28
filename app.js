90be5e044d12de65c835b5c914ddf8b24c6ea206
subhasree9812 <subhasreepadhi86@gmail.com>
2024-08-28 19:11:40 +0530

    <commit_message>

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
+const path = require("path");
+const methodOverride= require("method-override");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
@@ -15,22 +17,68 @@ async function main() {
     await mongoose.connect(MONGO_URL);
 }
 
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname,"views"));
+app.use(express.urlencoded({extended:true}));
+app.use(methodOverride("_method"));
+
+
 app.get("/", (req,res) =>{
     res.send("Hi I am root");
 });
 
-app.get("/testListing",async (req,res) =>{
-   let sampleListing = new Listing({
-    title: "My New Villa",
-    description: "By the beach",
-    price: 1200,
-    location: "Calangute, Goa",
-    country: "India",
-   });
-   await sampleListing.save();
-   console.log("sample was saved");
-   res.send("successful testing")
-});
+//Index Route
+app.get("/listings",async (req,res) =>{
+  const allListings = await Listing.find({});
+        res.render("listings/index.ejs",{allListings});
+    });
+
+    //New Route
+    app.get("/listings/new",(req,res) =>{
+        res.render("listings/new.ejs");
+    });
+
+    //Show Rout
+    app.get("/listings/:id",async (req,res) =>{
+        let {id} = req.params;
+        const listing = await Listing.findById(id);
+        res.render("listings/show.ejs",{listing});
+    });
+
+    //Create Route
+    app.post("/listings",async (req,res) =>{ 
+        const newListing = new Listing(req.body.listing);
+        await newListing.save();
+        res.redirect("/listings");
+    });
+
+
+    //Edit Route
+    app.get("/listings/:id/edit",async (req,res) =>{
+        let {id} = req.params;
+        const listing = await Listing.findById(id);
+        res.render("listings/edit.ejs",{listing});
+    })
+
+    //Update Route
+    app.put("/listings/:id", async(req,res) =>{
+        let {id} = req.params;
+        await Listing.findByIdAndUpdate(id,{...req.body.listing});
+        res.redirect("/listings");
+    })
+
+// app.get("/testListing",async (req,res) =>{
+//    let sampleListing = new Listing({
+//     title: "My New Villa",
+//     description: "By the beach",
+//     price: 1200,
+//     location: "Calangute, Goa",
+//     country: "India",
+//    });
+//    await sampleListing.save();
+//    console.log("sample was saved");
+//    res.send("successful testing")
+// });
 
 app.listen(8080, () =>{
     console.log("server is listening to port 8080");
--- models/listing.js
+++ models/listing.js
@@ -2,24 +2,28 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const listingSchema = new Schema({
-    title: {
-        type:String,
-        required: true,
+  title: {
+    type: String,
+    required: true,
+  },
+  description: String,
+  image: {
+    type: new Schema({
+      filename: { type: String },
+      url: {
+        type: String,
+        default:
+          "https://unsplash.com/photos/a-view-of-a-mountain-range-with-a-lake-in-the-foreground-ckcEoPdIcIY",
+      },
+    }),
+    default: {
+      filename: "default-image",
+      url: "https://unsplash.com/photos/a-view-of-a-mountain-range-with-a-lake-in-the-foreground-ckcEoPdIcIY",
     },
-    description: String,
-    image:{
-       type:String,
-       default:
-        "https://unsplash.com/photos/a-view-of-a-mountain-range-with-a-lake-in-the-foreground-ckcEoPdIcIY",
-       set: (v) => 
-        v === ""
-        ? "https://unsplash.com/photos/a-view-of-a-mountain-range-with-a-lake-in-the-foreground-ckcEoPdIcIY"
-        : v,
-    },
-    
-    price: Number,
-    location: String,
-    country: String,
+  },
+  price: Number,
+  location: String,
+  country: String,
 });
 
 const Listing = mongoose.model("Listing", listingSchema);
