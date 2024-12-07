cfbcef2f15aa6c5572bfcc622eff5542b2f10b64
subhasree9812 <subhasreepadhi86@gmail.com>
2024-12-07 11:35:57 +0530

    Added review model, updated app.js for review functionality, and improved UI in edit, index, and show views

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const ejsMate = require("ejs-Mate");
 const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
 const {listingSchema} = require("./schema.js");
+const Review = require("./models/review.js");
 
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
@@ -95,6 +96,23 @@ app.get("/listings",wrapAsync(async (req,res) =>{
         console.log(deletedListing);
         res.redirect("/listings");
     }));
+
+    //Reviews
+    //Post route
+    app.post("/listings/:id/reviews", async(req,res)=>{
+    let listing =await Listing.findById(req.params.id);
+    let newReview = new Review(req.body.review);
+
+    listing.reviews.push(newReview);
+
+    await newReview.save();
+    await listing.save();
+
+    console.log("new review saved");
+    res.send("new review saved");
+
+
+    })
 
 // app.get("/testListing",async (req,res) =>{
 //    let sampleListing = new Listing({
