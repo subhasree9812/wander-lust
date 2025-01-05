5b9ba4916a091a992685631efd46ca0709fe90a2
subhasree9812 <subhasreepadhi86@gmail.com>
2025-01-05 18:17:48 +0530

    Added new routes and updated views for listings

--- views/listings/listing.js
+++ views/listings/listing.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const Review = require("./review.js");
 
 const listingSchema = new Schema({
   title: {
@@ -31,5 +32,11 @@ const listingSchema = new Schema({
   ]
 });
 
+listingSchema
+.post("findOneAndDelete", async(listing) =>{
+  if(listing) {
+    await Review.deleteMany({_id : {$in:listing.reviews}})
+  }
+})
 const Listing = mongoose.model("Listing", listingSchema);
 module.exports = Listing;
