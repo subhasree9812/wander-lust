a1da343800b70c7850903fc9dea2235ae74bb122
subhasree9812 <subhasreepadhi86@gmail.com>
2025-01-07 21:33:21 +0530

    file added

---
+++ classroom/server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app= express();
+
+
+app.get("/", (req,res)=>{
+    res.send("Hi, I am root!")
+});
+
+app.listen(3000, () =>{
+    console.log("server is listening to 3000");
+});
