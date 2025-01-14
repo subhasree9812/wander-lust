bcafbb189720dc0402f14d25b60836c33c47dc22
subhasree9812 <subhasreepadhi86@gmail.com>
2025-01-14 10:52:49 +0530

    new files added

--- app.js
+++ app.js
@@ -12,8 +12,9 @@ const LocalStrategy = require("passport-local");
 const User = require("./models/user.js");
 
 
-const listings = require("./routes/listing.js");
-const reviews= require("./routes/review.js");
+const listingRouter = require("./routes/listing.js");
+const reviewRouter= require("./routes/review.js");
+const userRouter = require("./routes/user.js");
 
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
@@ -77,8 +78,9 @@ app.get("/demouser", async(req, res) =>{
     let registeredUser= await User.register(fakeuser, "helloworld");
     res.send(registeredUser);
 });
-app.use("/listings", listings);
-app.use("/listings/:id/reviews", reviews);
+app.use("/listings", listingRouter);
+app.use("/listings/:id/reviews", reviewRouter);
+app.use("/", userRouter);
 
 app.all("*",(req,res,next)=>{
     next(new ExpressError(404,"Page not found!"));
