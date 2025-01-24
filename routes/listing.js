4fafec5a0b53dac6ae183e1882cd2256bdd834e8
subhasree9812 <subhasreepadhi86@gmail.com>
2025-01-24 09:47:00 +0530

    Updated

--- routes/listing.js
+++ routes/listing.js
@@ -21,7 +21,7 @@ router.get("/",wrapAsync(async (req,res) =>{
       router.get("/:id",wrapAsync(async (req,res) =>{
           let {id} = req.params;
           const listing = await Listing.findById(id)
-          .populate({path:"reviews", populate:{path:"author"}})
+          .populate({path:"reviews", populate:{path:"author"},})
           .populate("owner");
           if(!listing){
             req.flash("error", "Listing you requested for does not exist.");
