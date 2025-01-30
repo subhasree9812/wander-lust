82e28d258d6656909e9dcc49959f54ea73dbf4e4
subhasree9812 <subhasreepadhi86@gmail.com>
2025-01-30 13:53:16 +0530

    Changes made

--- app.js
+++ app.js
@@ -19,10 +19,16 @@ const userRouter = require("./routes/user.js");
 // MongoDB Connection
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
-mongoose
-  .connect(MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log("Connected to MongoDB"))
-  .catch((err) => console.error("MongoDB connection error:", err));
+
+mongoose.connect('mongodb://localhost:27017/yourDatabaseName')
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('Could not connect to MongoDB:', err));
+
+
+// mongoose
+//   .connect(MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+//   .then(() => console.log("Connected to MongoDB"))
+//   .catch((err) => console.error("MongoDB connection error:", err));
 
 // Middleware and View Engine Setup
 app.set("view engine", "ejs");
