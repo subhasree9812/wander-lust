1a192a39b94b24b58ebe2f714ef368e19735af16
subhasree9812 <subhasreepadhi86@gmail.com>
2025-01-30 22:26:19 +0530

    Changes made

--- app.js
+++ app.js
@@ -1,3 +1,109 @@
+// const express = require("express");
+// const app = express();
+// const mongoose = require("mongoose");
+// const path = require("path");
+// const methodOverride = require("method-override");
+// const ejsMate = require("ejs-mate");
+// const ExpressError = require("./utils/ExpressError.js");
+// const session = require("express-session");
+// const flash = require("connect-flash");
+// const passport = require("passport");
+// const LocalStrategy = require("passport-local");
+// const User = require("./models/user.js");
+
+// // Routers
+// const listingRouter = require("./routes/listing.js");
+// const reviewRouter = require("./routes/review.js");
+// const userRouter = require("./routes/user.js");
+
+// // MongoDB Connection
+// const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+
+
+// mongoose.connect('mongodb://localhost:27017/yourDatabaseName')
+//   .then(() => console.log('Connected to MongoDB'))
+//   .catch(err => console.error('Could not connect to MongoDB:', err));
+
+
+// // mongoose
+// //   .connect(MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+// //   .then(() => console.log("Connected to MongoDB"))
+// //   .catch((err) => console.error("MongoDB connection error:", err));
+
+// // Middleware and View Engine Setup
+// app.set("view engine", "ejs");
+// app.set("views", path.join(__dirname, "views"));
+// app.engine("ejs", ejsMate);
+// app.use(express.urlencoded({ extended: true }));
+// app.use(methodOverride("_method"));
+// app.use(express.static(path.join(__dirname, "/public")));
+
+// // Session Configuration
+// const sessionOptions = {
+//   secret: "mysupersecretcode",
+//   resave: false,
+//   saveUninitialized: true,
+//   cookie: {
+//     expires: Date.now() + 7 * 24 * 60 * 60 * 1000, // 7 days
+//     maxAge: 7 * 24 * 60 * 60 * 1000,
+//     httpOnly: true,
+//   },
+// };
+
+// app.use(session(sessionOptions));
+// app.use(flash());
+
+// // Passport Configuration
+// app.use(passport.initialize());
+// app.use(passport.session());
+// passport.use(new LocalStrategy(User.authenticate()));
+// passport.serializeUser(User.serializeUser());
+// passport.deserializeUser(User.deserializeUser());
+
+// // Middleware for Flash Messages and Current User
+// app.use((req, res, next) => {
+//   res.locals.success = req.flash("success");
+//   res.locals.error = req.flash("error");
+//   res.locals.currUser = req.user; // Optional: Use to access logged-in user in templates
+//   next();
+// });
+
+// // Routes
+// app.get("/", (req, res) => {
+//   res.send("Hi, I am root");
+// });
+
+// // app.get("/demouser", async (req, res) => {
+// //   let fakeuser = new User({
+// //     email: "student@gmail.com",
+// //     username: "delta-student",
+// //   });
+
+// //   let registeredUser = await User.register(fakeuser, "helloworld");
+// //   res.send(registeredUser);
+// // });
+
+// app.use("/listings", listingRouter);
+// app.use("/listings/:id/reviews", reviewRouter);
+// app.use("/", userRouter);
+
+// // Error Handling
+// app.all("*", (req, res, next) => {
+//   next(new ExpressError(404, "Page not found!"));
+// });
+
+// app.use((err, req, res, next) => {
+//   const { statusCode = 500, message = "Something went wrong!" } = err;
+//   res.status(statusCode).render("error.ejs", { message });
+// });
+
+// // Start the Server
+// app.listen(8080, () => {
+//   console.log("Server is listening on port 8080");
+// });
+
+
+
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
@@ -19,16 +125,17 @@ const userRouter = require("./routes/user.js");
 // MongoDB Connection
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
+async function connectDB() {
+  try {
+    await mongoose.connect(MONGO_URL);
+    console.log("Connected to MongoDB");
+  } catch (err) {
+    console.error("Could not connect to MongoDB:", err);
+    process.exit(1); // Exit the application on DB connection failure
+  }
+}
 
-mongoose.connect('mongodb://localhost:27017/yourDatabaseName')
-  .then(() => console.log('Connected to MongoDB'))
-  .catch(err => console.error('Could not connect to MongoDB:', err));
-
-
-// mongoose
-//   .connect(MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-//   .then(() => console.log("Connected to MongoDB"))
-//   .catch((err) => console.error("MongoDB connection error:", err));
+connectDB();
 
 // Middleware and View Engine Setup
 app.set("view engine", "ejs");
@@ -73,16 +180,6 @@ app.get("/", (req, res) => {
   res.send("Hi, I am root");
 });
 
-// app.get("/demouser", async (req, res) => {
-//   let fakeuser = new User({
-//     email: "student@gmail.com",
-//     username: "delta-student",
-//   });
-
-//   let registeredUser = await User.register(fakeuser, "helloworld");
-//   res.send(registeredUser);
-// });
-
 app.use("/listings", listingRouter);
 app.use("/listings/:id/reviews", reviewRouter);
 app.use("/", userRouter);
@@ -98,6 +195,7 @@ app.use((err, req, res, next) => {
 });
 
 // Start the Server
-app.listen(8080, () => {
-  console.log("Server is listening on port 8080");
+const PORT = 8080;
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
 });
