03753822e5e2f4c6151e5b8dc035a6b326b1dd7a
subhasree9812 <subhasreepadhi86@gmail.com>
2025-02-01 13:04:36 +0530

    file added

--- routes/listing.js
+++ routes/listing.js
@@ -4,13 +4,10 @@ const wrapAsync = require("../utils/wrapAsync.js");
 const Listing = require("../models/listing.js");
 const{isLoggedIn, isOwner,validateListing} = require("../middleware.js");
 
-
+const listingController = require("../controllers/listings.js");
 
 //Index Route
-router.get("/",wrapAsync(async (req,res) =>{
-    const allListings = await Listing.find({});
-          res.render("listings/index.ejs",{allListings});
-      }));
+router.get("/",wrapAsync(listingController.index));
   
       //New Route
       router.get("/new",isLoggedIn, (req,res) =>{
